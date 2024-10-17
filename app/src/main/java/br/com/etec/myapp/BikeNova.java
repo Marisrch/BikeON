5ae6663ede45c9b0f175d5b5ddcf624a7ef205b7
Marisrch <marianasorrochedelima@gmail.com>
2024-10-17 19:52:44 -0300

    indo 4

---
+++ app/src/main/java/br/com/etec/myapp/BikeNova.java
@@ -0,0 +1,14 @@
+package br.com.etec.myapp;
+
+public class BikeNova {
+    private String titulo;
+    private int imagem;
+
+    //construtor da classe
+
+
+    public BikeNova(String titulo, int imagem) {
+        this.titulo = titulo;
+        this.imagem = imagem;
+    }
+}
